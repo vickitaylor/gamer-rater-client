b10d2b79393b482421504ef85952b7d2e878d713
Vicki Taylor <vtaylor85@live.com>
2022-08-05 14:44:32 -0500

    auth and nav bar added

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,39 @@
+import { Link, useNavigate } from "react-router-dom"
+import "./NavBar.css"
+
+export const NavBar = () => {
+    const navigate = useNavigate()
+    return (
+        <ul className="navbar">
+            <Link className="navbar__link" to="/games">
+                <li className="navbar__item">
+                    Games
+                </li>
+            </Link>
+            <Link className="navbar__link" to="/categories">
+                <li className="navbar__item">
+                    Categories
+                </li>
+            </Link>
+
+            {
+                (localStorage.getItem("lu_token") !== null) ?
+                    <li className="nav-item">
+                        <button className="nav-link fakeLink"
+                            onClick={() => {
+                                localStorage.removeItem("lu_token")
+                                navigate('/login')
+                            }}
+                        >Logout</button>
+                    </li> :
+                    <>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/login">Login</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/register">Register</Link>
+                        </li>
+                    </>
+            }        </ul>
+    )
+}
